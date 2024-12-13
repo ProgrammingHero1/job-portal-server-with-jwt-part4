b8376c103a7498321656d62be043ffb356cd4eab
Programming Hero <programming.hero1@gmail.com>
2024-12-13 16:49:32 -0600

    set token to http only cookie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 require('dotenv').config()
 
@@ -9,6 +10,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -37,8 +39,14 @@ async function run() {
         // Auth related APIs
         app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, 'secret', { expiresIn: '1h' });
-            res.send(token);
+            const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false, // http://localhost:5173/signIn
+                    sameSite: 'none'
+                })
+                .send({ success: true });
         })
 
         // jobs related APIs
